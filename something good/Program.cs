4839acadef60cdae25b289302a5bd54872eed8d2
Giorf81 <rejroberts7@gmail.com>
2024-02-23 12:10:02 +0300

    Добавьте файлы проекта.

---
+++ something good/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace something_good
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = Console.ReadLine();
+            ChangeName(ref MyName);
+        }
+
+
+        static void ChangeName(ref string Name)
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
